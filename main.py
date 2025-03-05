a323c7d69a284dcffab526b1d57189a502812f93
cmgross <chuckgross@gmail.com>
2025-03-05 15:16:39 -0500

    Added sprite groups

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
 import pygame
 from constants import *
-from circleshape import *
-from player import *
+from player import Player
 
 def main():
     pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_clock = pygame.time.Clock()
+   
     dt = 0
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable,drawable)
     current_player = Player((SCREEN_WIDTH / 2),(SCREEN_HEIGHT / 2) )
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        current_player.update(dt)
+        
+        
+        updateable.update(dt)
+        
         screen.fill((0, 0, 0))
-        current_player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+      
         pygame.display.flip()
+
+        #limit the framerate to 60 FPS
         dt = (game_clock.tick(60) / 1000)
        
 
--- player.py
+++ player.py
@@ -6,7 +6,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
        super().__init__(x,y,PLAYER_RADIUS)
        self.rotation = 0
-    
+       
 
     # in the player class
     def triangle(self):
@@ -26,7 +26,20 @@ class Player(CircleShape):
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
+        if keys[pygame.K_w]:
+            self.move(dt)
+
         if keys[pygame.K_a]:
             self.rotate(-dt)
+
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
         if keys[pygame.K_d]:
             self.rotate(dt)
+      
+        
+
+    def move(self,dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += (forward * PLAYER_SPEED * dt)
