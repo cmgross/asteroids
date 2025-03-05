707e169d0000977934ca0fd7a4ecc63d8c2d52c5
cmgross <chuckgross@gmail.com>
2025-03-05 14:45:13 -0500

    Added player class: player drawing on screen

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     pygame.init()
@@ -7,13 +9,19 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
+    current_player = Player((SCREEN_WIDTH / 2),(SCREEN_HEIGHT / 2) )
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        current_player.draw(screen)
         pygame.display.flip()
+        dt = (game_clock.tick(60) / 1000)
+       
 
 
 
