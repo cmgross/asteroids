a6153438e7be03e13426e2219597d1ce1fd85857
cmgross <chuckgross@gmail.com>
2025-03-06 10:29:22 -0500

    Added asteroid splitting

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def main():
             for bullet in shots:
                 if (asteroid.collides_with(bullet)):
                     bullet.kill()
-                    asteroid.kill()
+                    asteroid.split()
         
         screen.fill((0, 0, 0))
 
